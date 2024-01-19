cdb08f9c63b325e69eff5c5d0c66227ce4f22b49
imbalet <zhmaevva@gmail.com>
2024-01-19 15:14:52 +0300

    added libs, moved to ESP32

--- src/html.h
+++ src/html.h
@@ -1,275 +1,379 @@
 #include <Arduino.h>
 const char index_html[] PROGMEM = R"====(
-    <!DOCTYPE html>
-    <html lang="ru">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Станция управления</title>
-    </head>
-    <!--  -->
-    <style type="text/css">
-        body {
-            background-color: #292929;
-            color: aliceblue;
-        }
-    
-        meter {
-            height: 4vh;
-    
-        }
-    
-        #gamepads {
-            height: 100%;
-            display: flex;
-        }
-    
-        .gamepad {
-            background-color: #1a1a1a;
-            border: aliceblue;
-            width: 50%;
-            height: 10%;
-            display: flex;
-            align-items: flex-start;
-            flex-wrap: nowrap;
-            border-style: solid;
-            padding: 1%;
-    
-        }
-    
-        .axes {
-            /* width: 100%; */
-            height: 10%;
-            display: flex;
-            align-items: flex-start;
-            flex-wrap: nowrap;
-            flex-direction: column;
-            padding: 1%;
-            float: left;
-            text-wrap: nowrap;
-        }
-    
-        .buttons {
-            width: 100%;
-            height: 10%;
-            display: flex;
-            align-items: flex-start;
-            flex-wrap: wrap;
-            padding: 1%;
-            float: left;
-        }
-    
-        .button {
-            background-color: #1a1a1a;
-            border-style: solid;
-            padding: 5px;
-            margin: 10px;
-        }
-    
-        .pressed {
-            background-color: #107c10;
-        }
-    </style>
-    <!--  -->
-    
-    <body>
-        <p id="press">PRESS ANY BUTTON</p>
-        <div id="gamepads">
-            <!-- <div class="gamepad" id="gamepad0">
-                <div class="axes" id="axes0"></div>
-                <div class="buttons" id="buttons0"></div>
-            </div> -->
-    
-    
-        </div>
-        <div id="test"></div>
-    
-    </body>
-    <!--  -->
-    <script>
-        let controllers = {};
-        let firstGamepad = -1;
-    
-        var gateway = `ws://${window.location.hostname}:81/`;
-        var websocket;
-        window.addEventListener('load', onLoad);
-    
-        function connecthandler(e) {
-            //
-        }
-    
-        function remove(index) {
-            let axes = document.getElementById("axes" + index);
-            let buttons = document.getElementById("buttons" + index);
-            let del = document.getElementById("gamepad" + index);
-            document.getElementById("gamepads").removeChild(del);
-        }
-    
-        function disconnecthandler(e) {
-            //console.log(e)
-            remove(e.gamepad.index);
-            delete controllers[e.gamepad.index];
-        }
-    
-        function add(gamepad) {
-    
-            
-            document.getElementById("gamepads").innerHTML += `
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Станция управления</title>
+</head>
+<!--  -->
+<style type="text/css">
+    body {
+        background-color: #292929;
+        color: aliceblue;
+    }
+
+    meter {
+        height: 4vh;
+
+    }
+
+    #gamepads {
+        height: 100%;
+        display: flex;
+    }
+
+    .gamepad {
+        background-color: #1a1a1a;
+        border: aliceblue;
+        width: 50%;
+        height: 10%;
+        display: flex;
+        align-items: flex-start;
+        flex-wrap: nowrap;
+        border-style: solid;
+        padding: 1%;
+
+    }
+
+    .axes {
+        /* width: 100%; */
+        height: 10%;
+        display: flex;
+        align-items: flex-start;
+        flex-wrap: nowrap;
+        flex-direction: column;
+        padding: 1%;
+        float: left;
+        text-wrap: nowrap;
+    }
+
+    .buttons {
+        width: 100%;
+        height: 10%;
+        display: flex;
+        align-items: flex-start;
+        flex-wrap: wrap;
+        padding: 1%;
+        float: left;
+    }
+
+    .button {
+        background-color: #1a1a1a;
+        border-style: solid;
+        padding: 5px;
+        margin: 10px;
+    }
+
+    .pressed {
+        background-color: #107c10;
+    }
+
+    label {
+        display: block;
+        width: 30%;
+    }
+
+    input {
+        width: 20%;
+    }
+</style>
+<!--  -->
+
+<body>
+    <p id="press">PRESS ANY BUTTON</p>
+    <div id="gamepads"></div>
+    <div id="test"></div>
+    <div id="config"></div>
+    <div id="ipDiv">
+        <label>IP
+            <br>
+            <input type="text" id="ip" value="ws://${window.location.hostname}:81/">
+            <button onclick=changeIP()>
+                CONNECT
+            </button>
+            <hr>
+        </label>
+    </div>
+    <div id="debug">
+        <span id="send"></span>
+        <span id="scan"></span>
+        <span id="feedbackData"></span>
+    </div>
+</body>
+
+<script>
+    /*Global Variables*/
+    let controllers = {};
+
+    let params = {
+        sendInterval: 150,
+        scanInterval: 10,
+        feedback: true,
+        movementPad: 0,
+        manipulatorPad: 1,
+    };
+
+    var gateway = `ws://${window.location.hostname}:81/`;
+    var websocket;
+
+    let prevScanTime;
+
+    /*Gamepad connection*/
+    function remove(index) {
+        let axes = document.getElementById("axes" + index);
+        let buttons = document.getElementById("buttons" + index);
+        let del = document.getElementById("gamepad" + index);
+        document.getElementById("gamepads").removeChild(del);
+    }
+
+    function disconnecthandler(e) {
+        remove(e.gamepad.index);
+        delete controllers[e.gamepad.index];
+    }
+
+    function add(gamepad) {
+        document.getElementById("gamepads").innerHTML += `
             <div class="gamepad" id="gamepad${gamepad.index}">
+                ${gamepad.index}
                 <div class="axes" id="axes${gamepad.index}"></div>
                 <div class="buttons" id="buttons${gamepad.index}"></div>
             </div>
             `
-    
-    
-            let axes = document.getElementById("axes" + gamepad.index);
-            let buttons = document.getElementById("buttons" + gamepad.index);
-    
-            controllers[gamepad.index] = gamepad;
-    
-            //let indexG = gamepad.index == firstGamepad ? 0 : 1;
-    
-    
-            for (i = 0; i < gamepad.axes.length; i++) {
-                axes.innerHTML += `
+        let axes = document.getElementById("axes" + gamepad.index);
+        let buttons = document.getElementById("buttons" + gamepad.index);
+
+        controllers[gamepad.index] = gamepad;
+        if (Object.keys(controllers).length == 1) {
+            params.movementPad = gamepad.index;
+        }
+        if (Object.keys(controllers).length == 2) {
+            params.manipulatorPad = gamepad.index;
+        }
+        showConfig();
+
+        for (i = 0; i < gamepad.axes.length; i++) {
+            axes.innerHTML += `
                             <label>
                                 ${i} axis
                                 <meter id="${gamepad.index}ax${i}" min="-1" max="1" value="0"></meter>
                             <label>
                             <br>
                         `
-            }
-            for (i = 0; i < gamepad.buttons.length; i++) {
-                buttons.innerHTML += `
+        }
+        for (i = 0; i < gamepad.buttons.length; i++) {
+            buttons.innerHTML += `
                     <div class = "button" id = "${gamepad.index}bt${i}">
                         ${i}
                     </div>
                     `
-            }
         }
-    
-        function scan() {
-            let gamepads = navigator.getGamepads();
-    
-            for (i in gamepads) {
-                if (gamepads[i] != null) {
-                    if (Object.keys(controllers).length <= 1 && !(firstGamepad in controllers)) {
-                        firstGamepad = i;
-                    }
-                    if (gamepads[i].index in controllers) {
-                        controllers[gamepads[i].index] = gamepads[i];
-                    }
-                    else {
-                        add(gamepads[i]);
-                        //controllers[gamepads[i].index] = gamepads[i];
-                    }
-    
+    }
+
+    function scan() {
+        let gamepads = navigator.getGamepads();
+
+        for (i in gamepads) {
+            if (gamepads[i] != null) {
+                if (gamepads[i].index in controllers) {
+                    controllers[gamepads[i].index] = gamepads[i];
                 }
-    
-            }
-            update();
-            //console.log(controllers);
-            //console.log(firstGamepad);
-        }
-    
-        function update() {
-            for (i in controllers) {
-                var controller = controllers[i];
-                //console.log(controller);
-                for (var j = 0; j < controller.axes.length; j++) {
-                    document.getElementById(i + "ax" + j).value = controller.axes[j];
-                }
-                for (var j = 0; j < controller.buttons.length; j++) {
-                    if (controller.buttons[j].pressed) {
-                        document.getElementById(i + "bt" + j).classList.add("pressed")
-                    }
-                    else {
-                        document.getElementById(i + "bt" + j).classList.remove("pressed")
-                    }
+                else {
+                    add(gamepads[i]);
                 }
             }
         }
-    
-        function data() {
-            let data = "";
-            
-            for (i in controllers) {
-                var controller = controllers[i];
-                let ind = i == firstGamepad ? 0 : 1;
-                data += ind + "a";
-                for (var j = 0; j < controller.axes.length; j++) {
-                    data += String(Math.trunc(controller.axes[j] * 255) + 255).padStart(3, '0')
-                    //data += ind + "a" + (Math.trunc(controller.axes[j] * 255) + 255);
-                    //document.getElementById(i + "ax" + j).value = controller.axes[j];
-                }
-                data += "/";
-                data += ind + "b";
-                for (var j = 0; j < controller.buttons.length; j++) {
-                    let pressed = controller.buttons[j].pressed ? 1 : 0;
-                    data += pressed;// МБ переводить в hex чтобы меньше памяти жрало, надо смотреть.
-                    //if (controller.buttons[j].pressed) {
-                      //  document.getElementById(i + "bt" + j).classList.add("pressed")
-                }
-                data += "/";
+        update();
+
+        document.getElementById("scan").innerHTML = "scan" + String(prevScanTime) + "//" + String(Date.now() - prevScanTime)
+        prevScanTime = Date.now()
+        setTimeout(scan, params.scanInterval)
+    }
+
+    function update() {
+        for (i in controllers) {
+            var controller = controllers[i];
+            for (var j = 0; j < controller.axes.length; j++) {
+                document.getElementById(i + "ax" + j).value = controller.axes[j];
             }
-            document.getElementById("test").innerHTML = data;
-            
-            return data;
-    
-        }
-    
-        let wsConn = 0;
-        
-        function send() {
-            if(wsConn){
-                websocket.send(data());
+            for (var j = 0; j < controller.buttons.length; j++) {
+                if (controller.buttons[j].pressed) {
+                    document.getElementById(i + "bt" + j).classList.add("pressed")
+                }
+                else {
+                    document.getElementById(i + "bt" + j).classList.remove("pressed")
+                }
             }
-            //websocket.send(data());
         }
-        function initWebSocket() {
-            console.log('Trying to open a WebSocket connection...');
-            websocket = new WebSocket(gateway);
-            websocket.onopen = onOpen;
-            websocket.onclose = onClose;
-            websocket.onmessage = onMessage; // <-- add this line
+    }
+
+    /*Config functions*/
+    function feedback() {
+        let text = document.getElementById("feedbackData")
+        if (params.feedback) {
+            text.innerHTML = "feedback" + String(messTime) + "//" + String(Date.now() - messTime)
         }
-        function onOpen(event) {
-            wsConn = 1;
-            console.log('Connection opened');
+        else {
+            text.innerHTML = "feedback is off"
         }
-        function onClose(event) {
-            wsConn = 0;
-            console.log('Connection closed');
-            setTimeout(initWebSocket, 2000);
+    }
+
+    function changeIP() {
+        console.log("changed")
+        console.log("disconnect")
+        if (typeof websocket != "undefined") {
+            websocket.close()
         }
-        function onMessage(event) {
-            console.log(event.data);
+        gateway = document.getElementById("ip").value
+        initWebSocket()
+    }
+
+    function serializeForm(formNode) {
+        const { elements } = formNode
+        const data = Array.from(elements)
+            .map((element) => {
+                const { name, type } = element
+                const value = type === 'checkbox' ? element.checked : element.value
+
+                return { name, value }
+            })
+            .filter((item) => !!item.name)
+
+        console.log(data)
+        return data
+    }
+
+    function setParam(e) {
+        e.preventDefault();
+        let data = serializeForm(e.target);
+        for (i of data) {
+            params[i["name"]] = Number(i.value)
         }
-        function onLoad(event) {
-            initWebSocket();
+        console.log(params)
+        feedback()
+    }
+
+    function showConfig() {
+        document.getElementById("config").innerHTML = ""
+        let el = document.createElement("form");
+        //el.id = "form";
+        for (i in params) {
+            switch (typeof (params[i])) {
+                case ("number"):
+                    el.innerHTML +=
+                        `
+                                <label id= "${i}"  >${i}
+                                    <br>
+                                    <input type="text" name = ${i} value = "${params[i]}">
+                                    <hr>
+                                </label>
+                            `
+                    break;
+                case ("boolean"):
+                    el.innerHTML +=
+                        `
+                                <label id= "${i}">${i}
+                                    <input type="checkbox" value = "${Number(params[i])}" name = ${i} checked = "${Number(params[i])}">
+                                    <hr>
+                                </label>
+                            `
+                    break;
+                case ("string"):
+                    break;
+            }
         }
-    
-    
-    
-    
-        window.addEventListener("gamepadconnected", connecthandler);
-        window.addEventListener("gamepaddisconnected", disconnecthandler);
-    
-    
-        //    setInterval(function () {
-        //        update();
-        //    }, 10);
-    
-        setInterval(function () {
-            send();
-        }, 170);
-    
-        setInterval(function () {
-            scan();
-        }, 10);
-    </script>
-    <!--  -->
-    
-    </html>
+        el.innerHTML += `<input type="submit">`
+        config.appendChild(el);
+        el.addEventListener('submit', setParam);
+
+    }
+
+    /*Data*/
+    function data() {
+        let data = "";
+
+        for (i in controllers) {
+            var controller = controllers[i];
+            let ind = i == params.movementPad ? 0 : 1;
+            data += ind + "a";
+            for (var j = 0; j < controller.axes.length; j++) {
+                data += String(Math.trunc(controller.axes[j] * 255) + 255).padStart(3, '0')
+            }
+            data += "/";
+            data += ind + "b";
+            for (var j = 0; j < controller.buttons.length; j++) {
+                let pressed = controller.buttons[j].pressed ? 1 : 0;
+                data += pressed;
+            }
+            data += "/";
+        }
+        if (Object.keys(controllers).length == 1) {
+            data += "1a255255255255/1b000000000000000000/"
+        }
+        document.getElementById("test").innerHTML = `${Number(params.feedback)}%${data.length}/` + data;
+
+        return `${Number(params.feedback)}%${data.length}/` + data;
+
+    }
+
+    /*WebSocket*/
+    let wsConn = 0;
+    let prevSendTime;
+
+    function send() {
+        if (wsConn) {
+            websocket.send(data());
+        }
+        document.getElementById("send").innerHTML = "send" + String(prevSendTime) + "//" + String(Date.now() - prevSendTime)
+        prevSendTime = Date.now()
+        setTimeout(send, params.sendInterval)
+    }
+    function initWebSocket() {
+        if (gateway == "ws://:81/") {
+            console.log(gateway)
+            return
+        }
+        console.log('Trying to open a WebSocket connection...');
+        websocket = new WebSocket(gateway);
+        websocket.onopen = onOpen;
+        websocket.onclose = onClose;
+        websocket.onmessage = onMessage;
+        websocket.onerror = onError;
+    }
+    function onError(event) {
+        console.log(event.message)
+    }
+    function onOpen(event) {
+        wsConn = 1;
+        console.log('Connection opened');
+    }
+    function onClose(event) {
+        wsConn = 0;
+        console.log('Connection closed');
+        setTimeout(initWebSocket, 2000);
+    }
+    let messTime;
+    function onMessage(event) {
+        feedback()
+        messTime = Date.now();
+    }
+    function onLoad(event) {
+        document.getElementById("ip").value = gateway;
+        showConfig();
+        initWebSocket();
+    }
+
+    /*Other*/
+    window.addEventListener('load', onLoad);
+    //window.addEventListener("gamepadconnected", connecthandler);
+    window.addEventListener("gamepaddisconnected", disconnecthandler);
+
+    setTimeout(send, params.sendInterval)
+    setTimeout(scan, params.scanInterval)
+
+</script>
+
+</html>
 )====";
--- src/main.cpp
+++ src/main.cpp
@@ -1,124 +1,69 @@
+#ifdef ESP8266
 #include <ESP8266WiFi.h>
+#endif
+
+#ifdef ESP32
+#include <WiFi.h>
+#endif
+
 #include <ESP8266WebServer.h>
 #include <WebSocketsServer.h>
-#include <Hash.h>
 #include "html.h"
 
-const char* ssid = "вифи 412/1";
-const char* password = "FE4121FE";
+const char* ssid = "pen is";
+const char* password = "12345678";
+
+#define DEF_PAD_DATA "%72/0a255255255255/0b000000000000000000/1a255255255255/1b000000000000000000/"
+#define UART_TIMEOUT 300
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-struct gamepad{
-  int axes [4];
-  bool buttons [20];
-  //bool control; //0-movement 1-manipulator
-};
+String msg = "";
 
-gamepad pad0, pad1;
-
-gamepad pads [2] = {pad0, pad1};
-
-String msg;
-String prevMsg;
-
+bool connected = 0;
+uint32_t timer = 0;
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-
-switch(type) {
-  case WStype_DISCONNECTED:
-    //Serial.printf("[%u] Disconnected!\n", num);
-    Serial.println("Disconnected");
-    break;
-  case WStype_CONNECTED:
-  {
-    IPAddress ip = webSocket.remoteIP(num);
-    //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-    // send message to client
-    webSocket.sendTXT(num, "Connected");
-  }
-    break;
-  case WStype_TEXT:
-    msg = ((char*) payload);
-    //Serial.printf("[%u] get Text: %s\n", num, payload);
-    // send message to client
-    // webSocket.sendTXT(num, "message here");
-    // send data to all connected clients
-    // webSocket.broadcastTXT("message here");
-    break;
-  case WStype_BIN:
-    Serial.println("Binary");
-    //Serial.printf("[%u] get binary length: %u\n", num, length);
-    //hexdump(payload, length);
-    // send message to client
-    // webSocket.sendBIN(num, payload, length);
-    break;
-}
-
-}
-
-
-
-void convertData(char* data){ //        0a001001001001/0b0000000000000000000/a001001001001/0b000000000000000000/
-  Serial.print(data[1]);
-  bool now; //1 - axes, 0 - buttons
-  uint8_t pad;
-  if(!(data[1] == 'a' || data[1] == 'b')){
-    // Serial.print("here0");
-    // Serial.print(data[1] != 'a' );
-    // Serial.print(data[1] != 'b');
-    return;
-  } 
-  //Serial.print("here");
-  for (int i = 1; data[i] != '\0'; i++){
-      if (data[i] == '\0') break;
-    if(data[i] == 'a' || data[i] == 'b'){
-      now = data[i] == 'a' ? 1 : 0;
-      pad = data[i-1] - 48;
-      continue;
+  switch(type) {
+    case WStype_DISCONNECTED:
+      connected = 0;
+      //Serial.println("Disconnected");
+      break;
+    case WStype_CONNECTED:
+    {
+      connected = 1;
+      IPAddress ip = webSocket.remoteIP(num);
+      webSocket.sendTXT(num, "Connected");
     }
-    //Serial.print("here1");
-    for (int j = 0; data[i-1] != '/';){
-        if (data[i] == '/') break;
-      if(now){
-        pads[pad].axes[j] = (data[i] - 48) * 100 + (data[i+1] - 48) * 10 + (data[i+2] - 48); 
-        j++;
-        i += 3;
+      break;
+    case WStype_TEXT:
+      timer = millis();
+      msg = ((String)((char*) payload)).substring(1);
+      if ((char)payload[0] == '1'){
+        webSocket.sendTXT(num, "got");
       }
-      else if(!now){
-        pads[pad].buttons[j] = data[i] - 48;
-        j++;
-        i++;
-      }
-    }
+      break;
+    case WStype_BIN:
+      //Serial.println("Binary");
+      break;
   }
-  //Serial.print(pads[0].axes[0]);
 }
-
-
-void printintarr(int* arr, int len){
-    for (int i = 0; i<len; i++){
-        Serial.println(arr[i]);
-    }
-}
-
 
 void handleRoot(){
   server.send(200, "text/html", index_html);
 }
 
 
-
 void setup(){
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-  Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
   }
+  
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.begin(115200, SERIAL_8N1);
 
   Serial.println(WiFi.localIP());
 
@@ -126,25 +71,18 @@ void setup(){
   server.begin();
 }
 
-long timer;
-
 void loop() {
   server.handleClient();
   webSocket.loop();
   
   yield();
 
-  if (millis() - timer >= 100){
-    prevMsg = msg;
-    Serial.print("                              ");
+  if(millis() - timer >= UART_TIMEOUT){
+    Serial.print(DEF_PAD_DATA);
     Serial.print('\r');
-    convertData((char*)prevMsg.c_str());
-    Serial.print("   ");
-    Serial.print(pads[0].axes[0]);
-        Serial.print("   ");
-    Serial.print(pads[1].axes[0]);
-    Serial.print("   ");
-    Serial.print(ESP.getFreeHeap());
-    timer = millis();
+  }
+  else{
+    Serial.print(msg);
+    Serial.print('\r');
   }
 }
