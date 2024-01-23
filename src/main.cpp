c1ed5e6b354db4052acab45ca5fb576245d3dbd9
imbalet <zhmaevva@gmail.com>
2024-01-23 16:33:51 +0300

    added stm32 code

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 #include <WebSocketsServer.h>
 #include "html.h"
 
-const char* ssid = "pen is";
-const char* password = "12345678";
+const char *ssid = "pen is";
+const char *password = "12345678";
 
 #define DEF_PAD_DATA "%72/0a255255255255/0b000000000000000000/1a255255255255/1b000000000000000000/"
-#define UART_TIMEOUT 300
+#define UART_TIMEOUT 500
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
@@ -24,43 +24,48 @@ String msg = "";
 bool connected = 0;
 uint32_t timer = 0;
 
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
-  switch(type) {
-    case WStype_DISCONNECTED:
-      connected = 0;
-      //Serial.println("Disconnected");
-      break;
-    case WStype_CONNECTED:
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case WStype_DISCONNECTED:
+    connected = 0;
+    // Serial.println("Disconnected");
+    break;
+  case WStype_CONNECTED:
+  {
+    connected = 1;
+    IPAddress ip = webSocket.remoteIP(num);
+    webSocket.sendTXT(num, "Connected");
+  }
+  break;
+  case WStype_TEXT:
+    timer = millis();
+    msg = ((String)((char *)payload)).substring(1);
+    if ((char)payload[0] == '1')
     {
-      connected = 1;
-      IPAddress ip = webSocket.remoteIP(num);
-      webSocket.sendTXT(num, "Connected");
+      webSocket.sendTXT(num, "got");
     }
-      break;
-    case WStype_TEXT:
-      timer = millis();
-      msg = ((String)((char*) payload)).substring(1);
-      if ((char)payload[0] == '1'){
-        webSocket.sendTXT(num, "got");
-      }
-      break;
-    case WStype_BIN:
-      //Serial.println("Binary");
-      break;
+    break;
+  case WStype_BIN:
+    // Serial.println("Binary");
+    break;
   }
 }
 
-void handleRoot(){
+void handleRoot()
+{
   server.send(200, "text/html", index_html);
 }
 
-
-void setup(){
+void setup()
+{
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(1000);
   }
-  
+
   webSocket.begin();
   webSocket.onEvent(webSocketEvent);
   Serial.begin(115200, SERIAL_8N1);
@@ -71,18 +76,26 @@ void setup(){
   server.begin();
 }
 
-void loop() {
+uint32_t send_timer = 0;
+
+void loop()
+{
   server.handleClient();
   webSocket.loop();
-  
-  yield();
 
-  if(millis() - timer >= UART_TIMEOUT){
-    Serial.print(DEF_PAD_DATA);
-    Serial.print('\r');
-  }
-  else{
-    Serial.print(msg);
-    Serial.print('\r');
+  yield();
+  if (millis() - send_timer >= 50)
+  {
+    if (millis() - timer >= UART_TIMEOUT)
+    {
+      Serial.print(DEF_PAD_DATA);
+      Serial.print('\r');
+    }
+    else
+    {
+      Serial.print(msg);
+      Serial.print('\r');
+    }
+    send_timer = millis();
   }
 }
